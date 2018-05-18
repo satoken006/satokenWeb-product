b44c7dfa2acd35a12d0d1d54ff2f31af965db7e4
satoken912 <seat.soil.74196901@gmail.com>
2018-05-18 16:43:46 +0900

    Fix DB connection in PRODUCT

--- index.php
+++ index.php
@@ -2,89 +2,60 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="../css/style.css">
-    <script src="../main.js"></script>
     <script src="javascripts/product.js"></script>
     <title>制作物</title>
 </head>
 <body>
     <div class="nav fixed">
         <ul id="menubar">
-            <li><a href="http://satoken.nkmr.io/">HOME</a></li>
-            <li><a href="http://satoken.nkmr.io/profile/">PROFILE</a></li>
-            <li><a href="http://satoken.nkmr.io/research/">RESEARCH</a></li>
-            <li><a href="http://satoken.nkmr.io/product/">PRODUCT</a></li>
-            <li><a href="http://satoken.nkmr.io/activity/">ACTIVITY</a></li>
-            <li><a href="http://satoken.nkmr.io/link/">LINK</a></li>
+            <li><a href="https://satoken.nkmr.io/">HOME</a></li>
+            <li><a href="https://satoken.nkmr.io/profile/">PROFILE</a></li>
+            <li><a href="https://satoken.nkmr.io/research/">RESEARCH</a></li>
+            <li><a href="https://satoken.nkmr.io/product/">PRODUCT</a></li>
+            <li><a href="https://satoken.nkmr.io/activity/">ACTIVITY</a></li>
+            <li><a href="https://satoken.nkmr.io/link/">LINK</a></li>
         </ul>
     </div>
-    <img id="header" src="http://satoken.nkmr.io/img/header.jpg">
+    <img id="header" src="https://satoken.nkmr.io/img/header.jpg">
 
     <div id="content">
         <div class="heading">
-            講義・個人での制作 <b>2016</b>
+            講義・個人での制作 2016
         </div>
 
         <div class="achievement-list">
             <div class="activity-box">
-                <img src="../../img/activity/calendar.jpg">
-                <p>2014</p>
+                <a href="2014/">
+                    <img src="../../img/activity/calendar.jpg">
+                    <p>2014</p>
+                </a>
             </div>
             <div class="activity-box">
-                <img src="../../img/activity/calendar.jpg">
-                <p>2015</p>
+                <a href="2015/">
+                    <img src="../../img/activity/calendar.jpg">
+                    <p>2015</p>
+                </a>
             </div>
             <div class="activity-box">
-                <img src="../../img/activity/calendar.jpg">
-                <p>2016</p>
+                <a href="./">
+                    <img src="../../img/activity/calendar.jpg">
+                    <p>2016</p>
+                </a>
             </div>
         </div>
 
         <div id="item-list">
 
-            <div class="item">
-                <div class="item-heading">
-                    <img class="symbol" src="../img/product/handwriting.png">
-                    <div class="achievement-heading">metaMOJIphose</div>
-                </div>
-                <div class="item-desc">
-                    数式化された手書き文字同士ならば，単純な加重平均の計算を用いることで合成を行うことも可能です．<br>
-                    <a target="_blank" href="http://satoken912.github.io/metamojiphose/"><b>こちら</b></a>から体験できます．画数が同じでさえあれば何文字でも入力が可能です．
-                </div>
-                <div class="item-thumbnail">
-                    <img src="../img/product/thumbnail/metamojiphose.gif">
-                </div>
-                <div class="achievement-list">
-                    <div class="achievement-box">
-                        <a target="_blank" href="https://github.com/satoken912/metamojiphose">
-                            <img src="../../img/achievement/github.jpg">
-                            <p>ソース<br>コード</p>
-                        </a>
-                    </div>
-                </div>
-            </div>
+            <?php
 
-            <div class="item">
-                <div class="item-heading">
-                    <img class="symbol" src="../img/product/video.png">
-                    <div class="achievement-heading">インタラクティブアート「昔はいい時代だった・・・。」</div>
-                </div>
-                <div class="item-desc">
-                    B4での講義「メディアアート・デザイン」の課題として取り組んだ映像作品です．コーディングの作業をスーパーファミコンに使われていそうなインタフェースで表現したものとなります．
-                </div>
-                <div class="item-thumbnail">
-                    <img src="../img/product/thumbnail/good_era.png">
-                </div>
-                <div class="achievement-list">
-                    <div class="achievement-box">
-                        <a target="_blank" href="https://www.youtube.com/watch?v=1kRqRPWFjco">
-                            <img src="../../img/achievement/demovideo.jpg">
-                            <p>動画</p>
-                        </a>
-                    </div>
-                </div>
-            </div>
+            require "../php/function.php";
+            require "php/connectToMySQL.php";
+
+            showProducts($connection, 2016);
+
+            ?>
 
         </div>
 
--- javascripts/product.js
+++ javascripts/product.js
@@ -1,46 +1,17 @@
 $(function(){
 
-    $(".activity-box").click(function(){
-        var year = $(this).find("p").text();
-        $(".heading b").text( year );
+    $(".activity-box").mouseover(function(){
 
-        $.ajax({
-            type: "POST",
-            url: "./connect.php",
-            dataType: "text",
-            timeout: 1000,
-            data: {
-                year: year
-            }
+        $(this).css("background-color","#4ECCBF");
+        $(this).find("img").css("opacity", 0);
+        $(this).find("p").css({"background-color":"#4ECCBF", "color":"white"});
 
-        }).done(function(result){
-            // console.log("successed");
-            var item = result.split(";");
+    }).mouseout(function(){
 
-            $("#item-list").empty();
-            // それぞれのitemに対する処理
-            for(let i = 0; i < item.length-1; i ++){
-                var item_info = item[i].split(",");
+        $(this).css("background-color","#F5D0A9");
+        $(this).find("img").css("opacity", 0.5);
+        $(this).find("p").css({"background-color":"white", "color":"black", "opacity":0.85});
 
-                $item = $("<div class='item'></div>");
-                $itemHeading = $("<div class='item-heading'></div>");
-                $itemDesc = $("<div class='item-desc'></div>");
-                $itemThumbnail = $("<div class='item-thumbnail'></div>");
-
-                $itemHeading.append("<img class='symbol' src='../img/product/"+ item_info[1] +".png'>");
-                $itemHeading.append("<div class='achievement-heading'>"+ item_info[2]+"<div class='achievement-heading-date'>"+ item_info[0] +"</div></div>");
-                $itemDesc.append( item_info[3] );
-                $itemThumbnail.append( "<img src='"+ item_info[4] +"'>" );
-
-                $item.append($itemHeading);
-                $item.append($itemDesc);
-                $item.append($itemThumbnail);
-
-                $("#item-list").append( $item );
-            }
-        }).fail(function(result){
-            // console.log("failed");
-        });
     });
 
 });
